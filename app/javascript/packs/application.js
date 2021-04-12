232f58cbfb91e704f343f227fafffa426d7782b5
rohitsharma <rohit.sharma1986@live.com>
2021-04-12 12:50:51 +0100

    movie

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,23 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'bootstrap';
+import { initTyped } from '../plugins/init_typedjs.js';
+import { initSweetalert } from '../plugins/init_sweetalert';
+import { initSelect2 } from '../plugins/init_select2';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+document.addEventListener('turbolinks:load', () => {
+  // Call your JS functions here
+  // [...]
+  initTyped();
+  initSweetalert();
+  initSelect2();
+  AOS.init();
+});
---
+++ app/javascript/packs/plugins/init_sweetalert.js
@@ -0,0 +1,10 @@
+initSweetalert('#sweet-alert-demo', {
+  title: "Are you sure?",
+  text: "This action cannot be reversed",
+  icon: "warning"
+}, (value) => {
+  if (value) {
+    const link = document.querySelector('#delete-link');
+    link.click();
+  }
+});
